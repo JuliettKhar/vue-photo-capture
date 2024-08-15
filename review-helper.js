37620408d1311f369edb2206b21cf5d047a6e636
julia kharlamova <juliettkhar@gmail.com>
2024-08-15 15:20:51 +0300

    resolve conflicts

---
+++ review-helper.js
@@ -0,0 +1,25 @@
+import { Reviewer} from 'reviewer-lib';
+
+const apiKey = process.env.OPENAI_API_KEY;
+if (!apiKey) {
+  console.error('Error: OPENAI_API_KEY is not set');
+  process.exit(1);
+}
+
+const reviewer = new Reviewer(apiKey);
+let code = '';
+
+process.stdin.on('data', chunk => {
+  code += chunk;
+});
+process.stdin.on('end', () => {
+  reviewer.submitCode(code)
+    .then((feedback) => {
+      console.log('Code Review Feedback:');
+      console.log(feedback);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      process.exit(1);
+    });
+});
