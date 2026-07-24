a8f4f32ef5618fba6d3eb9ba0ee90fc4eabcc7af
julia_kh <juliettkhar@gmail.com>
2026-07-24 18:27:09 +0900

    update eslint

--- dist/vue-photo-capture.mjs
+++ dist/vue-photo-capture.mjs
@@ -1,6 +1,6 @@
 import { ref as r } from "vue";
 function d() {
-  const e = r(null), i = r(null), n = r(null), c = {
+  const t = r(null), i = r(null), n = r(null), c = {
     width: { max: 1280, ideal: 1280 },
     height: { min: 400, ideal: 1080 },
     facingMode: "user",
@@ -8,21 +8,21 @@ function d() {
     aspectRatio: { ideal: 1.7777777778 }
   };
   return {
-    videoForScreenShot: e,
+    videoForScreenShot: t,
     screenshotVideoBlob: i,
     videoStream: n,
     setUpVideoForScreenshot: async (o = c) => {
       try {
-        const t = await navigator.mediaDevices.getUserMedia({ video: o }), { width: s, height: a } = t.getVideoTracks()[0].getSettings();
-        e.value = document.createElement("video"), e.value.setAttribute("autoplay", "true"), e.value.setAttribute("playsinline", "true"), e.value.setAttribute("width", String(s || 1280)), e.value.setAttribute("height", String(a || 1280)), e.value.srcObject = t, n.value = t;
-      } catch (t) {
-        throw new Error(t?.message || t.toString());
+        const e = await navigator.mediaDevices.getUserMedia({ video: o }), { width: s, height: a } = e.getVideoTracks()[0].getSettings();
+        t.value = document.createElement("video"), t.value.setAttribute("autoplay", "true"), t.value.setAttribute("playsinline", "true"), t.value.setAttribute("width", String(s || 1280)), t.value.setAttribute("height", String(a || 1280)), t.value.srcObject = e, n.value = e;
+      } catch (e) {
+        throw new Error(e?.message || e.toString(), { cause: e });
       }
     },
-    capturePhoto: (o = e.value) => {
+    capturePhoto: (o = t.value) => {
       if (o) {
-        const t = document.createElement("canvas");
-        t.width = o.width, t.height = o.height, t.getContext("2d")?.drawImage(o, 0, 0, t.width, t.height), t.toBlob((a) => {
+        const e = document.createElement("canvas");
+        e.width = o.width, e.height = o.height, e.getContext("2d")?.drawImage(o, 0, 0, e.width, e.height), e.toBlob((a) => {
           i.value = a;
         });
       } else
